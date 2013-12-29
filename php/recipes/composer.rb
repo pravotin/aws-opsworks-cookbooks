713bfeb6aae48daa1043c7480133fdbd9bcb9e45
Gpraveen <gpraveen.reddy@gmail.com>
2013-12-29 09:41:09 -0800

    Checking for application type before installing composer

--- php/recipes/composer.rb
+++ php/recipes/composer.rb
@@ -1,10 +1,16 @@
-#Run PHP Composer for this PHP app.
-script "install_composer" do
-  interpreter "bash"
-  user "root"
-  cwd "#{deploy[:deploy_to]}/current"
-  code <<-EOH
-  curl -s https://getcomposer.org/installer | php
-  php composer.phar install -n --optimize-autoloader --prefer-source
-  EOH
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    next
+  end
+  #Run PHP Composer for this PHP app.
+  script "install_composer" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    php composer.phar install -n --optimize-autoloader --prefer-source
+    EOH
+  end
 end
