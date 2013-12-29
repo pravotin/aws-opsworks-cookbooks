e27443ec6c7f41a6443dcc0ebfbb0c954766c995
Gpraveen <gpraveen.reddy@gmail.com>
2013-12-28 23:37:58 -0800

    Using different ruby versions to ensure deploy doesnt fail

--- ruby/attributes/ruby.rb
+++ ruby/attributes/ruby.rb
@@ -1,12 +1,21 @@
 include_attribute 'opsworks_initial_setup::default'
 include_attribute 'opsworks_commons::default'
 
+# Pravotin 12/28 deploy is failing due to the following error: As of Userspace Ruby version is not 2.0.0p247 - found ruby 2.0.0p353 (2013-11-22 revision 43784) [x86_64-linux]
+# But on Ubuntu's ruby is still ruby 2.0.0p247 (2013-06-27 revision 41674) [x86_64-linux].  so this switch case. 
+case node["platform_family"]
+when "rhel"
+  default[:ruby][:patch] = 'p353'
+when "debian"
+  default[:ruby][:patch] = 'p247'
+end
+
 # ruby_version 1.8.7 is handled by 'ruby-enterprise'
 case node["opsworks"]["ruby_version"]
 when "2.0.0"
   default[:ruby][:major_version] = '2.0'
   default[:ruby][:full_version] = '2.0.0'
-  default[:ruby][:patch] = 'p247'
+  #default[:ruby][:patch] = 'p353'
   default[:ruby][:pkgrelease] = '1'
 when "1.9.3"
   default[:ruby][:major_version] = '1.9'
